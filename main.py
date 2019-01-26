75fdaa421f84423d8a019850748ddc536e78fea1
ayush <ayushnawal457@gmail.com>
2019-01-26 23:49:43 +0530

    Updated data loader

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/dataset-resized', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
