57917adda37283723e5c0f959b0e3f841c7692a1
ayush <ayushnawal457@gmail.com>
2019-01-29 22:06:13 +0530

    change class

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/dataset-resized', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/dataset-resized', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('cardboard', 'glass', 'metal', 'paper', 'plastic','trash')
 
 # Model
 print('==> Building model..')
