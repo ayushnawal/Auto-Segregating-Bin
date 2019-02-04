132d17653de0ba561599cc8db124957553bec8df
ayush <ayushnawal457@gmail.com>
2019-02-04 20:24:06 +0530

    Update data folder

--- main.py
+++ main.py
@@ -40,10 +40,10 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/dataset-resized', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/dataset-resized', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='./data/dataset-resized/', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('cardboard', 'glass', 'metal', 'paper', 'plastic','trash')
