cbaa1647137431a47652ea630ed4cef93c322e1e
ayush <ayushnawal457@gmail.com>
2019-02-11 00:46:57 +0530

    Update index in epoch

--- DatasetOperations.py
+++ DatasetOperations.py
@@ -56,3 +56,72 @@ def load_test(test_path, image_size):
   return X_test, X_test_id
 
 
+class DataSet(object):
+
+  def __init__(self, images, labels, ids, cls):
+    """Construct a DataSet. one_hot arg is used only if fake_data is true."""
+
+    self._num_examples = images.shape[0]
+
+
+    # Convert shape from [num examples, rows, columns, depth]
+    # to [num examples, rows*columns] (assuming depth == 1)
+    # Convert from [0, 255] -> [0.0, 1.0].
+
+    images = images.astype(np.float32)
+    images = np.multiply(images, 1.0 / 255.0)
+
+    self._images = images
+    self._labels = labels
+    self._ids = ids
+    self._cls = cls
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def images(self):
+    return self._images
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def ids(self):
+    return self._ids
+
+  @property
+  def cls(self):
+    return self._cls
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  def next_batch(self, batch_size):
+    """Return the next `batch_size` examples from this data set."""
+    start = self._index_in_epoch
+    self._index_in_epoch += batch_size
+
+    if self._index_in_epoch > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+
+      # # Shuffle the data (maybe)
+      # perm = np.arange(self._num_examples)
+      # np.random.shuffle(perm)
+      # self._images = self._images[perm]
+      # self._labels = self._labels[perm]
+      # Start next epoch
+
+      start = 0
+      self._index_in_epoch = batch_size
+      assert batch_size <= self._num_examples
+    end = self._index_in_epoch
+
+    return self._images[start:end], self._labels[start:end], self._ids[start:end], self._cls[start:end]
+
