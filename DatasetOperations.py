acd330892b1e9bd7124867cf425b8c367e69509b
ayush <ayushnawal457@gmail.com>
2019-02-10 21:19:59 +0530

    Add Dataset loader

---
+++ DatasetOperations.py
@@ -0,0 +1,34 @@
+import os
+import glob
+import numpy as np
+import cv2
+from sklearn.utils import shuffle
+
+def load_train(train_path, image_size, classes):
+    images = []
+    labels = []
+    ids = []
+    cls = []
+
+    print('Reading training images')
+    for fld in classes:   # assuming data directory has a separate folder for each class, and that each folder is named after the class
+        index = classes.index(fld)
+        print('Loading {} files (Index: {})'.format(fld, index))
+        path = os.path.join(train_path, fld, '*g')
+        files = glob.glob(path)
+        for fl in files:
+            image = cv2.imread(fl)
+            image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
+            images.append(image)
+            label = np.zeros(len(classes))
+            label[index] = 1.0
+            labels.append(label)
+            flbase = os.path.basename(fl)
+            ids.append(flbase)
+            cls.append(fld)
+    images = np.array(images)
+    labels = np.array(labels)
+    ids = np.array(ids)
+    cls = np.array(cls)
+
+    return images, labels, ids, cls
