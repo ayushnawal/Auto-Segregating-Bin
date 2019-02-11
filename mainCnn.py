908962d5a1ff18f5e8eb878b98f67fdf7f446fd3
ayush <ayushnawal457@gmail.com>
2019-02-12 04:03:01 +0530

    Add configuration and hyper-parameters

--- mainCnn.py
+++ mainCnn.py
@@ -14,3 +14,57 @@ from datetime import timedelta
 
 %matplotlib inline
 
+## CONFIGURATION
+
+# Convolutional Layer 1.
+filter_size1 = 3
+num_filters1 = 32
+
+# Convolutional Layer 2.
+filter_size2 = 3
+num_filters2 = 32
+
+# Convolutional Layer 3.
+filter_size3 = 3
+num_filters3 = 64
+
+# Fully-connected layer.
+fc_size = 128             # Number of neurons in fully-connected layer.
+
+# Number of color channels for the images: 1 channel for gray-scale.
+num_channels = 3
+
+# image dimensions (only squares for now)
+img_size = 128
+
+# Size of image when flattened to a single dimension
+img_size_flat = img_size * img_size * num_channels
+
+# Tuple with height and width of images used to reshape arrays.
+img_shape = (img_size, img_size)
+
+# class info
+classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic','trash']
+num_classes = len(classes)
+
+# batch size
+batch_size = 32
+
+# validation split
+validation_size = .16
+
+early_stopping = None  # use None if you don't want to implement early stoping
+
+train_path = 'data/dataset-resized/'
+# test_path = 'data/'
+# checkpoint_dir = "models/"
+
+## LOAD DATA
+
+data = dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
+test_images, test_ids = dataset.read_test_set(test_path, img_size)
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(data.train.labels)))
+print("- Test-set:\t\t{}".format(len(test_images)))
+print("- Validation-set:\t{}".format(len(data.valid.labels)))
