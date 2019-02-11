663ff7a5bd8818444550b7027003fb019ac9f6a3
ayush <ayushnawal457@gmail.com>
2019-02-12 04:14:26 +0530

    Add data loader

--- mainCnn.py
+++ mainCnn.py
@@ -68,3 +68,50 @@ print("Size of:")
 print("- Training-set:\t\t{}".format(len(data.train.labels)))
 print("- Test-set:\t\t{}".format(len(test_images)))
 print("- Validation-set:\t{}".format(len(data.valid.labels)))
+
+## Helper-function for plotting images
+
+def plot_images(images, cls_true, cls_pred=None):
+
+    if len(images) == 0:
+        print("no images to show")
+        return
+    else:
+        random_indices = random.sample(range(len(images)), min(len(images), 9))
+
+
+    images, cls_true  = zip(*[(images[i], cls_true[i]) for i in random_indices])
+
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(images[i].reshape(img_size, img_size, num_channels))
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+        # Show the classes as the label on the x-axis.
+        ax.set_xlabel(xlabel)
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+# Get some random images and their labels from the train set.
+
+images, cls_true  = data.train.images, data.train.cls
+
+# Plot the images and labels using our helper-function above.
+plot_images(images=images, cls_true=cls_true)
+
+
